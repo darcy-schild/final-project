8f02cfedf87c19f57feec8bb417490e4130f86e1
Darcy Schild <darcyschild@gmail.com>
2018-12-05 09:38:18 -0500

    update
    added cards, got jquery working for cards

---
+++ scripts/yoga.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(() => {
+
+
+    $('.card').hide();
+
+    $('#button1').on('click', () => {
+      $('.card').hide();
+       $('#card1').show();
+
+  });
+
+  $('#button2').on('click', () => {
+    $('.card').hide();
+     $('#card2').show();
+
+});
+
+$('#button3').on('click', () => {
+  $('.card').hide();
+   $('#card3').show();
+
+});
+
+
+$('#button4').on('click', () => {
+  $('.card').hide();
+   $('#card4').show();
+
+});
+});
