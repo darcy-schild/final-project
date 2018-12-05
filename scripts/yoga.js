324ca1d258ae79feb645a43a54b6c9a3a062a7b6
Darcy Schild <darcyschild@gmail.com>
2018-12-05 13:20:16 -0500

    update

--- scripts/yoga.js
+++ scripts/yoga.js
@@ -3,29 +3,44 @@
 $(document).ready(() => {
 
 
-    $('.card').hide();
+    $('#card1').hide();
+    $('#card2').hide();
+    $('#card3').hide();
+    $('#card4').hide();
 
     $('#button1').on('click', () => {
-      $('.card').hide();
+      $('.card1').hide();
+      $('.card2').hide();
+      $('.card3').hide();
+      $('.card4').hide();
        $('#card1').show();
 
   });
 
   $('#button2').on('click', () => {
-    $('.card').hide();
+    $('.card1').hide();
+    $('.card2').hide();
+    $('.card3').hide();
+    $('.card4').hide();
      $('#card2').show();
 
 });
 
 $('#button3').on('click', () => {
-  $('.card').hide();
+  $('.card1').hide();
+  $('.card2').hide();
+  $('.card3').hide();
+  $('.card4').hide();
    $('#card3').show();
 
 });
 
 
 $('#button4').on('click', () => {
-  $('.card').hide();
+  $('.card1').hide();
+  $('.card2').hide();
+  $('.card3').hide();
+  $('.card4').hide();
    $('#card4').show();
 
 });
